c119d5f5385fddf8c3be3d01bd692ef032ba66e0
myagmb28Dev <myagmb28s@gmail.com>
2026-05-20 17:32:05 +0900

    feat: update code from current diff

--- src/main/resources/static/js/api/dm-api.js
+++ src/main/resources/static/js/api/dm-api.js
@@ -21,7 +21,7 @@ export function createDmApi({ request, rawRequest, baseUrl }) {
       });
     },
     getMissingPets() {
-      return call("/api/missing-pets?page=0&size=50");
+      return call("/api/missing-pets?page=0&size=50&region=*");
     },
     getOwnMissingPets() {
       return call("/api/missing-pets?page=0&size=50&mineOnly=true");
